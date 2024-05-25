77418b150f4381d03972031eb3b9118222fe5ee2
rachanayadavv <154656967+rachanayadavv@users.noreply.github.com>
2024-05-25 13:05:07 +0530

    Add files via upload

---
+++ food-order/admin/index.php
@@ -0,0 +1,45 @@
+<html>
+    <head>
+        <title>Food Ordering Website-Home Page</title>   
+        <link rel="stylesheet" href="../css/admin.css" 
+    < /head >
+    <body>
+        <?php include('partials/menu.php');?>
+
+        <!--Main Content section Starts-->
+        <div class="main-content">
+            <div class="wrapper">
+              <h1>DASHBOARD</h1>
+
+              <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                Categories
+              </div>
+              <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                Categories
+              </div>
+              <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                Categories
+              </div>
+              <div class="col-4 text-center">
+                <h1>5</h1>
+                <br />
+                Categories
+              </div>
+
+              <div class="clearfix"></div>
+
+
+            </div>
+        </div>
+        <!--Main Content section ends-->
+
+        <?php include('partials/footer.php');?>
+
+    </body>
+</html>
---
+++ food-order/admin/partials/menu.php
@@ -0,0 +1,27 @@
+<?php include('../config/constants.php'); ?>
+
+<!-- menu.php -->
+<html>
+
+<head>
+    <title>Food Ordering Website-Home Page</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+    <!-- Menu Section Starts -->
+    <div class="menu text-center">
+        <div class="wrapper">
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="manage-admin.php">Admin</a></li>
+                <li><a href="manage-category.php">Category</a></li>
+                <li><a href="manage-food.php">Food</a></li>
+                <li><a href="manage-food.php">Order</a></li>
+            </ul>
+        </div>
+    </div>
+    <!-- Menu Section ends -->
+</body>
+
+</html>
